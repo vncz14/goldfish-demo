f343248e1845fd0b92be428f6e64e2faf914f814
Vincenzo <57498723+vncz14@users.noreply.github.com>
2025-06-08 18:12:13 -0400

    1.1/1.2 is default game option

--- src/App.vue
+++ src/App.vue
@@ -97,7 +97,7 @@ const getOutput = async () => {
         <h3>Game</h3>
         <select v-model="game">
             <option value="og_1.0">Wii Sports 1.0</option>
-            <option value="og_1.1">Wii Sports 1.1/1.2</option>
+            <option value="og_1.1" selected>Wii Sports 1.1/1.2</option>
             <option value="wsr">Wii Sports Resort</option>
         </select>
     </div>
